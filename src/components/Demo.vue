a90b83d7e4090b9a47b23858bd4953b8255d37d0
Zhuoooo <16620154495@163.com>
2022-02-19 22:19:52 +0800

    [feature] 新增表格滚动方法

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,9 +1,16 @@
 <template>
     <section class="draggable-demo">
-        <el-table :data="data" stripe height="300" style="width: 100%">
-            <el-table-column prop="date" label="Date" width="180" />
-            <el-table-column prop="name" label="Name" width="180" />
-            <el-table-column prop="address" label="Address" />
+        <el-table
+            :data="data"
+            ref="table"
+            stripe
+            height="300"
+            style="width: 100%"
+        >
+            <el-table-column type="index" />
+            <el-table-column prop="date" label="日期" width="180" />
+            <el-table-column prop="name" label="昵称" width="180" />
+            <el-table-column prop="address" label="地址" />
         </el-table>
     </section>
 </template>
@@ -17,14 +24,25 @@
 <script lang="ts" setup>
 import { ElTable, ElTableColumn } from 'element-plus';
 import Sortable from 'sortablejs';
-import { onMounted } from 'vue-demi';
+import { onMounted, ref } from 'vue-demi';
 import { tableData } from './data';
 
 const data: Record<string, any>[] = tableData;
+const table = ref(ElTable);
 
 onMounted(() => {
     const tbody = document.querySelector('.el-table tbody');
 
-    Sortable.create(tbody);
+    Sortable.create(tbody, {
+        onEnd() {
+            // 这里去发起请求
+            // 刷新列表
+            // console.log('end');
+        }
+    });
 });
+
+const scrollTop = (y: number) => {
+    table.value.$refs.scrollWrapper.setScrollTop(y);
+};
 </script>
