5e9453845460726b18a8e3bdacd121ebea9d38c4
Zhuoooo <16620154495@163.com>
2022-02-21 12:54:39 +0800

    [feature] 拖拽过程中添加样式

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,44 +5,100 @@
             ref="table"
             stripe
             height="300"
-            style="width: 100%"
+            :row-key="row => row.id"
         >
-            <el-table-column type="index" />
-            <el-table-column prop="date" label="日期" width="180" />
-            <el-table-column prop="name" label="昵称" width="180" />
-            <el-table-column prop="address" label="地址" />
+            <el-table-column width="100">
+                <template #default="scope">
+                    <span class="index">{{ scope.$index }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="date" label="日期" width="0.2" />
+            <el-table-column prop="name" label="昵称" width="0.2" />
+            <el-table-column prop="address" label="地址" width="0.4" />
+            <el-table-column label="操作" width="0.2">
+                <img
+                    class="draggable-table-row__handler"
+                    src="../assets/drag.png"
+                    title="按住拖动"
+                />
+            </el-table-column>
         </el-table>
     </section>
 </template>
 
-<style scoped>
+<style>
 .draggable-demo {
     height: 50vh;
+}
+.draggable-table-row__handler {
+    cursor: move;
+    cursor: grab;
+    margin-right: 16px;
+    width: 16px;
+    height: 16px;
+    user-select: none;
+}
+
+.draggable-table-row__handler:active {
+    cursor: grabbing;
+}
+.dragging-cursor {
+    cursor: grabbing;
+}
+.draggable-table-row__dragging {
+    display: table;
+    cursor: move;
+    box-shadow: 0 0 0 1px #e0e0e0, 0 0 8px 0 rgba(0, 0, 0, 0.3);
+    border-radius: 4px;
+    background: transparent;
 }
 </style>
 
 <script lang="ts" setup>
 import { ElTable, ElTableColumn } from 'element-plus';
 import Sortable from 'sortablejs';
-import { onMounted, ref } from 'vue-demi';
+import { onMounted, ref } from 'vue';
 import { tableData } from './data';
 
-const data: Record<string, any>[] = tableData;
-const table = ref(ElTable);
+let data = ref(tableData);
+const table = ref<Record<string, any>[]>([]);
+const ua = navigator.userAgent.toLowerCase();
 
 onMounted(() => {
     const tbody = document.querySelector('.el-table tbody');
 
     Sortable.create(tbody, {
+        animation: 180,
+        delay: 0,
+        handle: '.draggable-table-row__handler',
+        forceFallback: true,
+        fallbackClass: 'draggable-table-row__dragging',
+        ghostClass: 'dragging-cursor',
+        scrollSensitivity: 40, // 当拖拽到可滚动区域最上/下方 40 px的地方时，自动向上/下滚动
         onEnd() {
+            // 不管有没有进行拖拽移动，都会触发
+        },
+        onMove() {
+        },
+        onUpdate() {
             // 这里去发起请求
             // 刷新列表
-            // console.log('end');
-        }
+            refreshTable();
+        },
+        setData: ua.includes('firefox')
+            ? (dataTransfer: any) => {
+                  // firefox 拖拽时会打开新窗口, 需要规避
+                  dataTransfer.setData('noText', '');
+                  dataTransfer.effectAllowed = 'move';
+              }
+            : null
     });
 });
 
-const scrollTop = (y: number) => {
-    table.value.$refs.scrollWrapper.setScrollTop(y);
+const refreshTable = async () => {
+    setTimeout(() => {
+        let ran = Math.max((tableData.length * Math.random()) >> 0, 2);
+        data.value = tableData.slice(0, ran).reverse();
+    }, 300);
 };
 </script>
--- src/components/data.ts
+++ src/components/data.ts
@@ -1,102 +1,122 @@
 export const tableData = [
     {
-        date: '1992-12-10',
-        name: '韩勇',
-        address: '辽宁省'
+        "id": "AeCE3Cde-51ed-5EFA-9c4D-2F22FcA2EbAe",
+        "date": "1974-08-05",
+        "name": "冯强",
+        "address": "台湾"
     },
     {
-        date: '1996-04-30',
-        name: '林娟',
-        address: '上海'
+        "id": "7ACeBE64-6EBB-3514-116F-A73d51C7eDeB",
+        "date": "2001-01-23",
+        "name": "郑强",
+        "address": "福建省"
     },
     {
-        date: '1975-10-08',
-        name: '金平',
-        address: '宁夏回族自治区'
+        "id": "CC8B2E42-13De-d9d8-d58f-Eb5dA139aB2F",
+        "date": "2014-09-25",
+        "name": "周涛",
+        "address": "黑龙江省"
     },
     {
-        date: '2018-07-19',
-        name: '韩洋',
-        address: '新疆维吾尔自治区'
+        "id": "c284dACF-85cE-eE8B-D832-fEFFbd4E48Df",
+        "date": "1983-03-11",
+        "name": "贺霞",
+        "address": "陕西省"
     },
     {
-        date: '1980-11-20',
-        name: '郑杰',
-        address: '北京'
+        "id": "E6df95e3-c5dB-3F3E-b731-afF7Dd1C3eeA",
+        "date": "1976-12-19",
+        "name": "傅强",
+        "address": "甘肃省"
     },
     {
-        date: '1982-06-09',
-        name: '于平',
-        address: '山东省'
+        "id": "fEaf255b-AACB-ab91-82E7-09d2DB73873F",
+        "date": "1987-09-30",
+        "name": "姚平",
+        "address": "广东省"
     },
     {
-        date: '1981-11-14',
-        name: '邱艳',
-        address: '新疆维吾尔自治区'
+        "id": "4ceF4A26-adCE-1b3E-C71e-112628624DB4",
+        "date": "1999-08-20",
+        "name": "姜涛",
+        "address": "山西省"
     },
     {
-        date: '2015-09-02',
-        name: '姜霞',
-        address: '香港特别行政区'
+        "id": "aE74304d-0754-A2D6-847A-AfbAB05BfF8a",
+        "date": "2007-01-11",
+        "name": "文超",
+        "address": "辽宁省"
     },
     {
-        date: '2012-02-21',
-        name: '任平',
-        address: '台湾'
+        "id": "3aFeDf17-5df1-E9EE-FD7D-c2cECd15659c",
+        "date": "2003-04-15",
+        "name": "廖秀兰",
+        "address": "辽宁省"
     },
     {
-        date: '1989-07-15',
-        name: '汤秀英',
-        address: '黑龙江省'
+        "id": "d2b5f15d-44be-3f15-D7F5-545316dE6Adf",
+        "date": "1984-01-12",
+        "name": "蔡丽",
+        "address": "贵州省"
     },
     {
-        date: '2018-03-09',
-        name: '马勇',
-        address: '江苏省'
+        "id": "c2d5b3BF-271B-e7b7-4e60-c9BACBf9B1B0",
+        "date": "1989-03-05",
+        "name": "彭洋",
+        "address": "贵州省"
     },
     {
-        date: '1996-02-02',
-        name: '梁军',
-        address: '吉林省'
+        "id": "6B547F68-5c78-99d6-fD15-1ff39724e2ee",
+        "date": "2003-01-24",
+        "name": "孔超",
+        "address": "湖北省"
     },
     {
-        date: '1972-02-17',
-        name: '孙超',
-        address: '福建省'
+        "id": "8f9EB448-7D54-9AE5-fF14-17d87BbdF957",
+        "date": "1994-06-30",
+        "name": "文丽",
+        "address": "山西省"
     },
     {
-        date: '1980-02-17',
-        name: '方刚',
-        address: '山西省'
+        "id": "44dc4f1d-7EBe-CC7E-E1Ed-CdFAd8CCA125",
+        "date": "2000-04-08",
+        "name": "蔡娜",
+        "address": "香港特别行政区"
     },
     {
-        date: '1993-12-31',
-        name: '侯艳',
-        address: '澳门特别行政区'
+        "id": "A42cf7Be-9F3D-d63f-A84D-3BAB0C392f33",
+        "date": "2000-06-02",
+        "name": "郭敏",
+        "address": "上海"
     },
     {
-        date: '1988-04-08',
-        name: '钱涛',
-        address: '吉林省'
+        "id": "CCC0d9eb-dC97-d3CA-9B74-E19ABD4154bA",
+        "date": "1978-01-02",
+        "name": "乔霞",
+        "address": "黑龙江省"
     },
     {
-        date: '2017-07-18',
-        name: '常娜',
-        address: '内蒙古自治区'
+        "id": "dfeB61A7-A404-C30f-0eBA-4b3b9EbC69a4",
+        "date": "2015-02-20",
+        "name": "郭磊",
+        "address": "青海省"
     },
     {
-        date: '1975-06-21',
-        name: '胡娜',
-        address: '青海省'
+        "id": "dA188dcc-3bcF-cF72-EF98-C1E11Bed671F",
+        "date": "1997-07-08",
+        "name": "曾勇",
+        "address": "湖北省"
     },
     {
-        date: '1977-05-01',
-        name: '蒋娟',
-        address: '广东省'
+        "id": "43afbD9C-75CC-BEF8-7A6d-de02fCee3Afd",
+        "date": "2020-10-25",
+        "name": "彭伟",
+        "address": "安徽省"
     },
     {
-        date: '2008-08-31',
-        name: '朱艳',
-        address: '吉林省'
+        "id": "f68ce437-fdb7-eABF-97Ec-38Ff5219BdD7",
+        "date": "2004-01-20",
+        "name": "蒋军",
+        "address": "四川省"
     }
 ];
