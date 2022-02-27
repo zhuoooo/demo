8df262d6c8b580bd77c350767646d8f20a13fbfb
Zhuoooo <16620154495@163.com>
2022-02-27 23:34:51 +0800

    [add] 新增穿梭树组件

--- src/components/TransferTree.vue
+++ src/components/TransferTree.vue
@@ -1,5 +1,52 @@
-<template></template>
+<template>
+    <ElTree :data="leftData" :show-checkbox="true"></ElTree>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ElTree } from 'element-plus';
+import { watch, ref, onMounted } from 'vue';
+
+export interface TransferTreeNode {
+    label: string;
+    state?: 'none' | 'all' | 'part';
+    path?: string;
+    disabled?: boolean;
+    parent?: TransferTreeNode;
+    children?: TransferTreeNode[];
+}
+
+interface TransferTreeProps {
+    data: TransferTreeNode[];
+}
+
+const props = defineProps<TransferTreeProps>();
+
+let leftData = ref<TransferTreeNode[]>([]);
+const watchData = watch(
+    () => props.data,
+    val => {
+        leftData.value = initLeftData(val);
+    }
+);
+
+onMounted(() => {
+    leftData.value = initLeftData(props.data);
+});
+
+function initLeftData(val: TransferTreeNode[]) {
+    const cas = (tree: TransferTreeNode, parentPath: string) => {
+        tree.state = 'all';
+        tree.path = parentPath;
+
+        tree.children?.forEach((item, index) =>
+            cas(item, `${parentPath}.children[${index}]`)
+        );
+    };
+
+    val.forEach((item, index) => cas(item, `[${index}]`));
+
+    return val;
+}
+</script>
 
 <style scoped></style>
--- src/demo/TransferTree.vue
+++ src/demo/TransferTree.vue
@@ -1,3 +1,18 @@
-<template>TransferTree Demo</template>
+<template>
+    <TransferTree :data="data"></TransferTree>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import TransferTree from '../components/TransferTree.vue';
+
+const data = [
+    {
+        label: '全部',
+        children: [
+            {
+                label: '子组1'
+            }
+        ]
+    }
+];
+</script>
