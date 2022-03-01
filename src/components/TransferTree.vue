4c094964fdb261305005394c5c41dbce81d58a4e
Zhuoooo <16620154495@163.com>
2022-03-01 23:50:34 +0800

    [update] 补充transferTree接收配置

--- src/components/TransferTree.vue
+++ src/components/TransferTree.vue
@@ -1,10 +1,23 @@
 <template>
-    <ElTree :data="leftData" :show-checkbox="true"></ElTree>
+    <section class="transfer-tree">
+        <ElTree
+            class="left-tree"
+            :data="leftData"
+            v-bind="leftTreeOptions"
+        ></ElTree>
+        <ElTree
+            class="right-tree"
+            :data="rightData"
+            :filter-node-method="filterRightTree"
+            v-bind="rightTreeOptions"
+        ></ElTree>
+    </section>
 </template>
 
 <script lang="ts" setup>
 import { ElTree } from 'element-plus';
-import { watch, ref, onMounted } from 'vue';
+import { TreeComponentProps } from 'element-plus/es/components/tree/src/tree.type';
+import { watch, ref, onMounted, computed } from 'vue';
 
 export interface TransferTreeNode {
     label: string;
@@ -15,22 +28,49 @@ export interface TransferTreeNode {
     children?: TransferTreeNode[];
 }
 
+type Left = Partial<Omit<TreeComponentProps, 'data'>>;
+type Right = Partial<
+    Omit<TreeComponentProps, 'data' | 'showCheckbox' | 'filterNodeMethod'>
+>;
+
 interface TransferTreeProps {
     data: TransferTreeNode[];
+    leftBind?: Left;
+    rightBind?: Right;
 }
 
 const props = defineProps<TransferTreeProps>();
 
 let leftData = ref<TransferTreeNode[]>([]);
+let rightData = ref<TransferTreeNode[]>([]);
+
+const leftTreeOptions = computed(() => {
+    return {
+        nodeKey: 'id',
+        showCheckbox: true,
+        ...props.leftBind
+    };
+});
+
+const rightTreeOptions = computed(() => {
+    return {
+        ...leftTreeOptions.value,
+        showCheckbox: false,
+        ...props.rightBind
+    };
+});
+
 const watchData = watch(
     () => props.data,
     val => {
         leftData.value = initLeftData(val);
+        rightData.value = val;
     }
 );
 
 onMounted(() => {
     leftData.value = initLeftData(props.data);
+    rightData.value = props.data;
 });
 
 function initLeftData(val: TransferTreeNode[]) {
@@ -47,6 +87,19 @@ function initLeftData(val: TransferTreeNode[]) {
 
     return val;
 }
+
+function filterRightTree(value: string, data: any) {
+    return data.state !== 'none';
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.transfer-tree {
+    display: flex;
+
+    .left-tree,
+    .right-tree {
+        flex: 1;
+    }
+}
+</style>
--- src/demo/TransferTree.vue
+++ src/demo/TransferTree.vue
@@ -7,10 +7,50 @@ import TransferTree from '../components/TransferTree.vue';
 
 const data = [
     {
-        label: '全部',
+        id: 1,
+        label: 'Level one 1',
         children: [
             {
-                label: '子组1'
+                id: 4,
+                label: 'Level two 1-1',
+                children: [
+                    {
+                        id: 9,
+                        label: 'Level three 1-1-1'
+                    },
+                    {
+                        id: 10,
+                        label: 'Level three 1-1-2'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        label: 'Level one 2',
+        children: [
+            {
+                id: 5,
+                label: 'Level two 2-1'
+            },
+            {
+                id: 6,
+                label: 'Level two 2-2'
+            }
+        ]
+    },
+    {
+        id: 3,
+        label: 'Level one 3',
+        children: [
+            {
+                id: 7,
+                label: 'Level two 3-1'
+            },
+            {
+                id: 8,
+                label: 'Level two 3-2'
             }
         ]
     }
